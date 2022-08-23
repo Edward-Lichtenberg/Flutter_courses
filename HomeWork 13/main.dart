a2efeee4712987ff9ddeef38fcab38da652ea87e
Edward-Lichtenberg <109383928+Edward-Lichtenberg@users.noreply.github.com>
2022-08-24 04:47:13 +0600

    Added Home Work 13 on Cubes v:1.0
    Использовали MaterialApp, Scaffold, AppBar, Text, Padding, Column, Row, Container

--- HomeWork 13/main.dart
+++ HomeWork 13/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blue,
       ),
       body: const Cubes(),
-      //backgroundColor: const Color.fromARGB(255, 162, 223, 252),
+      backgroundColor: const Color.fromARGB(255, 162, 223, 252),
     );
   }
 }
@@ -48,154 +48,73 @@ class Cubes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 250),
-                child: Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      width: 75,
-                      height: 75,
-                      color: Colors.red,
-                      child: const Text(
-                        '1',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                      child: const Text(
-                        '2',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 120,
-                      height: 120,
-                      color: Colors.green,
-                      child: const Text(
-                        '3',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5, 50, 0, 0),
-                child: Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      width: 75,
-                      height: 75,
-                      color: Colors.red,
-                      child: const Text(
-                        '1',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                      child: const Text(
-                        '2',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 120,
-                      height: 120,
-                      color: Colors.green,
-                      child: const Text(
-                        '3',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5, 250, 0, 0),
-                child: Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      width: 75,
-                      height: 75,
-                      color: Colors.red,
-                      child: const Text(
-                        '1',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                      child: const Text(
-                        '2',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 120,
-                      height: 120,
-                      color: Colors.green,
-                      child: const Text(
-                        '3',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      padding: const EdgeInsets.all(20),
+      child: Row(children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [MyWidget()],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [MyWidget()],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const [MyWidget()],
+        ),
+      ]),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Column(children: [
+        Container(
+          alignment: Alignment.center,
+          width: 75,
+          height: 75,
+          color: Colors.red,
+          child: const Text(
+            '1',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
           ),
-        ],
-      ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          color: Colors.yellow,
+          child: const Text(
+            '2',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: 120,
+          height: 120,
+          color: Colors.green,
+          child: const Text(
+            '3',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }

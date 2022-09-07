867cc5eb38a92294bb91f2ae6346c2fbb27de7a5
Edward-Lichtenberg <109383928+Edward-Lichtenberg@users.noreply.github.com>
2022-09-07 10:51:58 +0600

    Added HomeWork 17 - Finish to the end!
    PageView может создавать эффект «карусели изображений».

---
+++ HomrWork 17/PageView.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'PageView',
+      home: MyStatelessWidget(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController();
+    return PageView(
+      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+      /// Use [Axis.vertical] to scroll vertically.
+      controller: controller,
+      children: const <Widget>[
+        Center(
+          child: Text('First Page'),
+        ),
+        Center(
+          child: Text('Second Page'),
+        ),
+        Center(
+          child: Text('Third Page'),
+        ),
+      ],
+    );
+  }
+}
